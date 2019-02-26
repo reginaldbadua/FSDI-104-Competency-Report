f2302384597b8ae12391f20e8fc4fe3bb234aeb6
reginaldbadua <46497568+reginaldbadua@users.noreply.github.com>
2019-02-26 07:16:32 -0800

    Add files via upload

---
+++ oop.js
@@ -0,0 +1,70 @@
+//Create an inventory desktops and laptops
+//Functionality: add, remove, consult
+
+class Computer{
+    // const name;
+    // const model;
+    // const brand;
+    
+    constructor(name, model, brand){
+    this.name = name; 
+    this.model = model;
+    this.brand = brand; 
+    
+  }
+    
+}
+
+class Laptop extends Computer{
+  // const screenSize; 
+
+  constructor (screenSize){
+    super(name,model,brand);
+    this.screenSize = screenSize;
+  }
+}
+
+class Desktop extends Computer{
+  // const size; 
+
+  constructor (size){
+    super(name,model,brand); //model is not defined
+    this.size = size;
+  }
+
+}
+class Inventory {
+  constructor() {
+  this.Inventory = [];
+  }
+  addComputer(model){
+  this.Inventory.push(model);
+  }
+
+  consultInventory(){
+  let z;
+  for (let i=0; i<this.Inventory.length; i++){
+    z = Object.values(this.Inventory[i]);
+    alert(z)
+    }
+  } 
+  removeFromInventory(model){
+  let x;
+  for (let i=0; i<this.Inventory.length; i++){
+    if(this.Inventory[i].model == model){
+      x = this.Inventory.splice(i,1);
+      } 
+    }
+  }
+}
+var HP = new Desktop('Omen','Ryzen','HP','Full Tower');
+var Dell = new Desktop ('Inspiron','Intel','Dell','Small');
+var Apple = new Laptop ('Macbook','Radeon','Apple','15-inch');
+var Lenovo = new Laptop ('Thinkpad','Intel', 'Lenovo', '14-inch');
+
+const inventory = new Inventory();
+
+inventory.consultInventory(HP);
+inventory.addComputer(HP);
+inventory.addComputer(Lenovo);
+inventory.removeFromInventory(HP);
